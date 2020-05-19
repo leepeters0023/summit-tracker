c58eb11d8aed811aa5ebf948f6b49b654bc3b4e5
Lee Peters <lpeters0023@gmail.com>
2020-05-19 17:08:55 -0400

    decoding polyline using leaflet encoded

--- intersect.js
+++ intersect.js
@@ -2,10 +2,12 @@ import * as turf from '@turf/turf'
 import lineIntersect from '@turf/line-intersect'
 import intersects from '@turf/intersect'
 import env from dotenv 
+import encoded from encoded
 
 let line1 = turf.lineString([[126, -11], [129, -21]]); // can be a linestring or polygon
 let line2 = turf.lineString([[123, -18], [131, -14]]);
 let intersects = turf.lineIntersect(line1, line2); // returns a feature collection of intersecting points this will be a boolean for me
 
 let stuff = curl -X GET "https://www.strava.com/api/v3/activities/3472159994" -H "accept: application/json" -H `authorization: Bearer ${STRAVA_KEY.env}`
-stuff.polyline = "udknGdmz}LGu@c@EsBJmA@oAHe@Ae@@gA?c@D_@Ai@Dg@?oALoA@g@JQX?PBx@LnABzAHp@FNd@Hb@AtC]|BM~KK`@MBw@EeGMyEGWCCc@?cCJqA@gAHkBDmAAi@DcA?ECUi@Cu@C{ByAWkAIO_@Ew@Ka@ODO[CA_@TaAv@_@T_A|@[\\"
+//stuff.polyline = "udknGdmz}LGu@c@EsBJmA@oAHe@Ae@@gA?c@D_@Ai@Dg@?oALoA@g@JQX?PBx@LnABzAHp@FNd@Hb@AtC]|BM~KK`@MBw@EeGMyEGWCCc@?cCJqA@gAHkBDmAAi@DcA?ECUi@Cu@C{ByAWkAIO_@Ew@Ka@ODO[CA_@TaAv@_@T_A|@[\\"  // strava returns this in Google encoded polyline algorithm format.
+let encoded = fromEncoded(resObj.polyline).geti
