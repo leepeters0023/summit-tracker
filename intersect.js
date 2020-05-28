61c24722d829b701ad8c1684aa53a623a40079b1
Lee Peters <lpeters0023@gmail.com>
2020-05-28 13:02:56 -0400

    firebase auth

--- intersect.js
+++ intersect.js
@@ -1,13 +1,20 @@
 import * as turf from '@turf/turf'
-import lineIntersect from '@turf/line-intersect'
-import intersects from '@turf/intersect'
-import env from dotenv 
-import encoded from encoded
+//import env from dotenv 
+import encoded from polyline-encoded
 
-let line1 = turf.lineString([[126, -11], [129, -21]]); // can be a linestring or polygon
-let line2 = turf.lineString([[123, -18], [131, -14]]);
+// rewrite this get request to
+//let stuff = curl -X GET "https://www.strava.com/api/v3/activities/3472159994" -H "accept: application/json" -H `authorization: Bearer ${STRAVA_KEY.env}`
+//stuff.polyline = "udknGdmz}LGu@c@EsBJmA@oAHe@Ae@@gA?c@D_@Ai@Dg@?oALoA@g@JQX?PBx@LnABzAHp@FNd@Hb@AtC]|BM~KK`@MBw@EeGMyEGWCCc@?cCJqA@gAHkBDmAAi@DcA?ECUi@Cu@C{ByAWkAIO_@Ew@Ka@ODO[CA_@TaAv@_@T_A|@[\\"  // strava returns this in Google encoded polyline algorithm format.
+//let encoded = fromEncoded(resObj.polyline)
+
+let encoded = "udknGdmz}LGu@c@EsBJmA@oAHe@Ae@@gA?c@D_@Ai@Dg@?oALoA@g@JQX?PBx@LnABzAHp@FNd@Hb@AtC]|BM~KK`@MBw@EeGMyEGWCCc@?cCJqA@gAHkBDmAAi@DcA?ECUi@Cu@C{ByAWkAIO_@Ew@Ka@ODO[CA_@TaAv@_@T_A|@["
+let line1 = L.polyline.fromEncoded(encoded).getLatLngs();
+console.log(line1)
+//line1 = turf.lineString(line1)
+let line2 = turf.lineString([[44.465332, -73.214800],[44.4653014,-73.2153133],[44.4650257,-73.2151658],[44.4648946,-73.2148868]])
+
 let intersects = turf.lineIntersect(line1, line2); // returns a feature collection of intersecting points this will be a boolean for me
 
-let stuff = curl -X GET "https://www.strava.com/api/v3/activities/3472159994" -H "accept: application/json" -H `authorization: Bearer ${STRAVA_KEY.env}`
-//stuff.polyline = "udknGdmz}LGu@c@EsBJmA@oAHe@Ae@@gA?c@D_@Ai@Dg@?oALoA@g@JQX?PBx@LnABzAHp@FNd@Hb@AtC]|BM~KK`@MBw@EeGMyEGWCCc@?cCJqA@gAHkBDmAAi@DcA?ECUi@Cu@C{ByAWkAIO_@Ew@Ka@ODO[CA_@TaAv@_@T_A|@[\\"  // strava returns this in Google encoded polyline algorithm format.
-let encoded = fromEncoded(resObj.polyline).geti
+if (intersects) {
+  console.log('true')
+}
